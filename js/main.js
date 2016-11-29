16c2cd6a51e6353c489a5a533d3cc70097302524
TheZ3ro <io@thezero.org>
2016-11-29 14:35:07 +0100

    Download page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ String.prototype.capitalize = function() {
   if (OSName != "unknown") {
     var button = document.getElementById("download_button");
     button.innerText = button.innerText.replace("Download","Download for "+OSName.capitalize());
+    button.innerHTML = '<i class="fa fa-'+OSName+'" aria-hidden="true"></i>' + button.innerHTML;
     button.href = button.href+"/#"+OSName;
     var other_platform = document.getElementById("platform_button");
     other_platform.style.display = "inline-block";
